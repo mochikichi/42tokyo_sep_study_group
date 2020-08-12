2213bab94475f5d353aebe58fc8b9a4720e49a99
mochikichi <mochikichi@live.jp>
2020-08-12 17:08:42 +0900

    add comment

--- 6/problem_3.c
+++ 6/problem_3.c
@@ -13,6 +13,7 @@ int main(void)
     printf("入力形式：N Y\n");
     scanf("%d %d", &N, &Y);
 
+    // 2重ループ(N+1)^2  N=2000だと、2000^2=4,000,000通り
     for (i = 0; i <= N; i++) {
         for (j = 0; j <= (N - i); j++) {
             k = N - i - j;
